82a7acba6850b5006295986af7b358fa0398fb19
Binal Yadav <binalyadav@Binals-MacBook-Pro.local>
2020-08-01 00:50:16 -0300

    made user module

--- v_server/models.py
+++ v_server/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
-class Users(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
+# class Users(models.Model):
+#     first_name = models.CharField(max_length=30)
+#     last_name = models.CharField(max_length=30)
 
-    def __str__(self):
-        return self.first_name
+#     def __str__(self):
+#         return self.first_name
--- v_server/serializers.py
+++ v_server/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from django.contrib.auth.models import User, Group
+from django.contrib.auth.hashers import *
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
@@ -8,7 +9,9 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         fields = ['username', 'first_name', 'last_name', 'email']
     def to_internal_value(self, data):
         instance = super(UserSerializer, self).to_internal_value(data)
-        instance["password"] = data['first_name'] + "@" + data['last_name']
+        passwordStr = data['first_name'] + "@" + data['last_name']
+        encryptedPassword = make_password(passwordStr)
+        instance["password"] = encryptedPassword
         return instance
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
